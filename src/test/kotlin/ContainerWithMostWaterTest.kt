8794f460d314d03529ff8e7bb2a4fc846e8903b8
dach83 <dach83@mail.ru>
2023-01-25 08:26:44 +0300

    Container With Most Water

---
+++ src/test/kotlin/ContainerWithMostWaterTest.kt
@@ -0,0 +1,23 @@
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+
+class ContainerWithMostWaterTest {
+
+    private fun checkAnswer(expected: Int, height: IntArray) {
+        val sut = ContainerWithMostWater()
+
+        val actual = sut.maxArea(height)
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun example1() {
+        checkAnswer(expected = 49, height = intArrayOf(1, 8, 6, 2, 5, 4, 8, 3, 7))
+    }
+
+    @Test
+    fun example2() {
+        checkAnswer(expected = 1, height = intArrayOf(1, 1))
+    }
+}
