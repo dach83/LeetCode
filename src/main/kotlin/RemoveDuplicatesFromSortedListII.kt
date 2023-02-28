0cd14584a55400d48b293a7f4bb3ed296e29b9e0
dach83 <dach83@mail.ru>
2023-02-28 08:11:02 +0300

    Remove Duplicates from Sorted List II

---
+++ src/main/kotlin/RemoveDuplicatesFromSortedListII.kt
@@ -0,0 +1,24 @@
+/**
+ * Problem description on [LeetCode](https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/)
+ */
+class RemoveDuplicatesFromSortedListII {
+
+    fun deleteDuplicates(head: ListNode?): ListNode? {
+        val start = ListNode()
+        var last = start
+
+        var prev: ListNode? = null
+        var curr = head
+        while (curr != null) {
+            if (prev?.`val` != curr.`val` && curr.`val` != curr.next?.`val`) {
+                last.next = curr
+                last = curr
+            }
+            prev = curr
+            curr = curr.next
+            last.next = null
+        }
+
+        return start.next
+    }
+}
